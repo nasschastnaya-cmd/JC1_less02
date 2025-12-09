4e28ece3d18775ccc199e7aae106c95396d924e9
nasschastnaya-cmd <nasschastnaya@gmail.com>
2025-12-09 19:57:58 +0300

    Add files via upload

---
+++ SimpleConsoleOutput1/src/edu/training/jc/simple_output/FormattingWithinSttring.java
@@ -0,0 +1,10 @@
+package edu.training.jc.simple_output;
+
+public class FormattingWithinSttring {
+
+	public static void main(String[] args) {
+		System.out.println("1) Todoist - баланс между мощностью и простотой.\n2) TickTick - встроенные календари и таймеры.\n3) Microsoft to do - для опытных пользователей Microsoft.");
+
+	}
+
+}
---
+++ SimpleConsoleOutput1/src/edu/training/jc/simple_output/FrameOutput.java
@@ -0,0 +1,10 @@
+package edu.training.jc.simple_output;
+
+public class FrameOutput {
+
+	public static void main(String[] args) {
+		System.out.println("***********\n*    *    *\n*    *    *\n*    *    *\n***********");
+
+	}
+
+}
